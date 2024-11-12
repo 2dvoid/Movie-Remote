c7424bfeb6ef327ba929d2b3cf8c8a81eed5d3aa
2dvoid <ghost@ghost>
2024-11-12 12:51:11 +0600

    Fixed syntax errors in #define

--- movie-remote.ino
+++ movie-remote.ino
@@ -1,10 +1,10 @@
 // Define Keycodes
-#define META = 0x08;
-#define CTRL = 0x01;
-#define SHIFT = 0x02;
-#define ALT = 0x04;
-#define META_CTRL = 0x09;
-#define META_ALTR = 0x0C;
+#define META 0x08;
+#define CTRL 0x01;
+#define SHIFT 0x02;
+#define ALT 0x04;
+#define META_CTRL 0x09;
+#define META_ALTR 0x0C;
 // Define the Keyboard Buffer
 /* A keyboard buffer is typically 8 bytes long. The first byte holds the modifier keys (such as Ctrl, Shift, Alt). The second byte is reserved and usually set to 0. The third to eighth bytes are used for regular keys, with each byte representing one key. */
 uint8_t buf[8] = {0};
