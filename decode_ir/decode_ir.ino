f75279520d6cd0ba9d680db7c291791ac064fcb3
2dvoid <ghost@ghost>
2024-11-17 20:06:14 +0600

    WIP

--- decode_ir/decode_ir.ino
+++ decode_ir/decode_ir.ino
@@ -9,9 +9,7 @@ void setup()
 
 void loop() {
   if (IrReceiver.decode()) {
-      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
-      //IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
-      //IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+      Serial.println(IrReceiver.decodedIRData.decodedRawData); // Print "old" raw data
       IrReceiver.resume(); // Enable receiving of the next value
       delay (100);
   }
--- test.ino
+++ test.ino
@@ -1,23 +1,27 @@
 #include <IRremote.hpp>
 #define IR_PIN 7
-// Define Keycodes
 
-//Modifier Keys:
-#define META 0x08;
-#define CTRL 0x01;
-#define SHIFT 0x02;
-#define ALT 0x04;
-#define META_CTRL 0x09;
-#define META_ALTR 0x0C;
-// Normal Keys:
-#define ENTER 0x28;
+// Combines Ir code with its corresponding Key code
+struct codemap {
+    uint32_t ircode;
+    uint8_t keycode;
+};
+
+// Array of codes
+struct codemap code [] = {
+    {F5, 0x80},
+    {F9, 0x81},
+    {F9, 0x81},
+    {F9, 0x81}
+}
 
 // Define the Keyboard Buffer
 /* A keyboard buffer is typically 8 bytes long. The first byte holds the modifier keys (such as Ctrl, Shift, Alt). The second byte is reserved and usually set to 0. The third to eighth bytes are used for regular keys, with each byte representing one key. */
 // Initially Set the buffer to 0
 uint8_t buf[8] = {0};
-uint8_t ircode;
 
+// Size of the code[] array:
+const int arraysize = sizeof (code) / sizeof (codemap);
 
 void setup () {
     Serial.begin(9600);
@@ -28,28 +32,31 @@ void setup () {
 void loop () {
 
     if (IrReceiver.decode()) {
-        ircode = IrReceiver.decodedIRData.decodedRawData; 
-        Serial.println (ircode);
-        IrReceiver.resume();
-    }
 
+        uint32_t recv_code = IrReceiver.decodedIRData.decodedRawData;
+        // Loop through the array and check which ir code matches the recieved ir code
+        for (int i=0; i<arraysize; i++) {
+
+            if (recv_code == code[i].ircode) {
+                keypress (code[i].keycode);
+                break;
+            }
+
+        }
+    }
 
 }
 
 
 // Function to Send Keypress
-// Argument: (Modifier_Key, Normal key)
-void keypress(uint8_t mod, uint8_t key) 
+void keypress(uint8_t key) 
 {
-    // Set the modifier keys in the first byte of the buffer
-    buf[0] = mod;
-    // Set the letter key to third byte of the buffer
+    // Set the key to third byte of the buffer
     buf[2] = key;
     // Send the buffer to simulate the keypress
     Serial.write(buf, 8);
     
     // Release the keys by resetting the changed buffer and sending it again
-    buf[0] = 0;
     buf[2] = 0; 
     Serial.write(buf, 8); 
     // Small Delay to ensure release is registered
